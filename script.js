6dcb2ed7c6f6475978258594e47ddc6a275bdd78
sonianb <sonia.botsarova@gmail.com>
2022-02-21 22:31:17 +0000

    display basic question one

--- script.js
+++ script.js
@@ -2,3 +2,26 @@ const questionOne = document.getElementById('country-question-one');
 const questionTwo = document.getElementById('country-question-two');
 const answerOne = document.getElementById('country-answer-one');
 const answerTwo = document.getElementById('country-answer-two');
+
+fetch(`https://restcountries.com/v3.1/all`)
+.then((response) => {
+  if (!response.ok) {
+    const error = new Error(response.status);
+    throw error;
+  }
+  else {
+    return response.json();
+  }
+})
+.then((data) => {
+    const countries = data.map(elem => {
+        return {country: elem.name.common, capital: elem.capital}
+    });
+     questionOne.innerText = `What is the capital of ${countries[45].country}?`
+     
+    //display answer
+    
+})
+
+//create function to display random country with capital 
+
