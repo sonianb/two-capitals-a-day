acd0a4581a9defed47f41ddbbb8328497e3c7137
sonianb <sonia.botsarova@gmail.com>
2022-02-21 21:58:46 +0000

    select DOM elements for questions and answers

---
+++ script.js
@@ -0,0 +1,4 @@
+const questionOne = document.getElementById('country-question-one');
+const questionTwo = document.getElementById('country-question-two');
+const answerOne = document.getElementById('country-answer-one');
+const answerTwo = document.getElementById('country-answer-two');
