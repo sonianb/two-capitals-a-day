63ac57b5139732dc9c5c7f880d96119a27bf1b83
sonianb <sonia.botsarova@gmail.com>
2022-02-22 16:30:06 +0000

    display random country for first question

--- script.js
+++ script.js
@@ -4,28 +4,35 @@ const answerOne = document.getElementById('country-answer-one');
 const answerTwo = document.getElementById('country-answer-two');
 
 fetch(`https://restcountries.com/v3.1/all`)
-.then((response) => {
-  if (!response.ok) {
-    const error = new Error(response.status);
-    throw error;
-  }
-  else {
-    return response.json();
-  }
-})
-.then((data) => {
+  .then((response) => {
+    if (!response.ok) {
+      const error = new Error(response.status);
+      throw error;
+    }
+    else {
+      return response.json();
+    }
+  })
+  .then((data) => {
     const countries = data.map(elem => {
-        return {country: elem.name.common, capital: elem.capital}
+      return {country: elem.name.common, capital: elem.capital}
     });
-    console.log(countries);
-     questionOne.innerText = `What is the capital of ${countries[5].country}?`
-     answerOne.innerText = `${countries[5].capital}` 
-    
-})
+    const countryObject = randomCountry(countries);
+    questionOne.innerText = `What is the capital of ${countryObject.country}?`
+    answerOne.innerText = `${countryObject.capital}`
+
+  })
 
 //create function to display random country
+function randomCountry(arr) {
+  return arr[Math.floor(Math.random() * arr.length)]
+}
 
-//test user input against answer 
+//display user input
+
+//test user input against answer and provide feedback if correct or not
+
+//display second question only after the input to the first one has been submitted
 
 
 
